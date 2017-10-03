4961d3407b9ab1bd6aa9312efab7786887703316
Nikolay Yushkevich <n.yushkevich@hotmail.com>
2017-10-03 20:56:07 +0300

    Correct install mistake

--- Formula/iroha.rb
+++ Formula/iroha.rb
@@ -17,8 +17,8 @@ class Iroha < Formula
     def install
       system "cmake -H. -Bbuild"
       system "cmake --build build -- -j8"
-      bin.install "mv .build/bin/irohad"
-      bin.install "mv .build/bin/iroha-cli"
+      bin.install "build/bin/irohad"
+      bin.install "build/bin/iroha-cli"
     end
   
     test do
