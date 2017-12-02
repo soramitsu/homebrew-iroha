284be7779bed6c326bab153c3fe0e438a4bb5e8f
Nikolay Yushkevich <n.yushkevich@hotmail.com>
2017-12-02 07:46:16 +0300

    Revert to old build
    Signed-off-by: Nikolay Yushkevich <n.yushkevich@hotmail.com>

--- Formula/iroha.rb
+++ Formula/iroha.rb
@@ -1,31 +1,29 @@
 class Iroha < Formula
-    desc "Hyperledger Iroha — distributed ledger technology platform"
-    homepage "http://iroha.tech/en"
-    url "https://github.com/hyperledger/iroha.git", :branch => "develop"
-    version "v0.95_preview-2"
+  desc "Hyperledger Iroha — distributed ledger technology platform"
+  homepage "http://iroha.tech/en"
+  url "https://github.com/hyperledger/iroha.git", :branch => "develop"
+  head "https://github.com/hyperledger/iroha.git"
+  version "v0.95_preview-2"
+   
+  depends_on "cmake" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "golang" => :build
+  depends_on "libtool"
+  depends_on "tbb"
+  depends_on "postgres"
+  depends_on "boost"
+  depends_on "grpc"
 
-    depends_on "libtool"
-    depends_on "tbb"
-    depends_on "postgres"
-    depends_on "boost"
-    depends_on "grpc"
-  
-    resource "iroha_cli" do
-      url "https://github.com/hyperledger/iroha/releases/download/v0.95_preview-2/iroha-cli-mac"
-      sha256 "85f177a641b5c636237c7271b69aca530d1aa444766325e5c67eddcf2c0a26a1"
-    end
-
-    resource "irohad" do
-      url "https://github.com/hyperledger/iroha/releases/download/v0.95_preview-2/irohad-mac"
-      sha256 "8ab188a77eaf11492df4540698309490485be3b4470778aa50639009d17e4e0f"
-    end
-
-    def install
-      resource("irohad").stage { bin.install "irohad-mac" }
-      resource("iroha_cli").stage { bin.install "iroha-cli-mac" }
-    end
-  
-    test do
-        system "#{bin}/iroha-cli-mac", "--help"
-    end
+  def install
+    system "cmake -H. -Bbuild"
+    system "cmake --build build -- -j8 irohad iroha-cli"
+    bin.install "build/bin/irohad"
+    bin.install "build/bin/iroha-cli"
+    pkgshare.install "build/libs"
   end
+
+  test do
+      system "#{bin}/iroha-cli", "--help"
+  end
+end
