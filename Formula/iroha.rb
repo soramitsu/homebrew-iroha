bd49325c4c57ef75ee4c9488ff42a4d8fb7f51a9
Nikolay Yushkevich <n.yushkevich@hotmail.com>
2017-12-02 06:57:16 +0300

    Change hash
    Signed-off-by: Nikolay Yushkevich <n.yushkevich@hotmail.com>

--- Formula/iroha.rb
+++ Formula/iroha.rb
@@ -12,12 +12,12 @@ class Iroha < Formula
   
     resource "iroha_cli" do
       url "https://github.com/hyperledger/iroha/releases/download/v0.95_preview-2/iroha-cli-mac"
-      sha1 "8eff9f5ccba6a040e79575c68e8f5940d461d0f2"
+      sha256 "85f177a641b5c636237c7271b69aca530d1aa444766325e5c67eddcf2c0a26a1"
     end
 
     resource "irohad" do
       url "https://github.com/hyperledger/iroha/releases/download/v0.95_preview-2/irohad-mac"
-      sha1 "8eff9f5ccba6a040e79575c68e8f5940d461d0f2"
+      sha256 "8ab188a77eaf11492df4540698309490485be3b4470778aa50639009d17e4e0f"
     end
 
     def install
